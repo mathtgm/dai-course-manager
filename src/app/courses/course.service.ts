c00612914d571448af7d238c3ca321dc5b9d9da0
Matheus Gabriel <matheusg663@gmail.com>
2022-07-18 21:44:44 -0300

    Requisições de HTTP(Criar, Alterar e Consultar)

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -1,24 +1,30 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { Course } from "./course";
 
 @Injectable({
     providedIn: 'root',
 })
 export class CourseService {
-    
-    retriveAll(): Course[] {
-        return COURSES;
+
+private coursesUrl: string = 'http://localhost:3100/api/courses';
+
+    constructor(private httpClient: HttpClient) {}
+
+    retriveAll(): Observable<Course[]> {
+        return this.httpClient.get<Course[]>(this.coursesUrl);
     }
 
-    retriveById(id: number): Course {
-        let course = COURSES.find((intereatorCourse: Course) => intereatorCourse.id === 1);
-        return course!;
+    retriveById(id: number): Observable<Course> {
+       return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
     }
 
-    save(course: Course): void {
+    save(course: Course): Observable<Course> {
         if(course.id) {
-            const index = COURSES.findIndex((courseIntereator: Course) => courseIntereator.id === course.id);
-            COURSES[index] = course
+            return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course);
+        } else {
+            return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
         }
     }
 }
